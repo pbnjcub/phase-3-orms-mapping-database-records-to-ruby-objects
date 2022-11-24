bd04396704be4fc035868ffddc867b30cde957ea
Paul Jung <pbnjcub@gmail.com>
2022-11-23 23:56:51 -0500

    lab done

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,40 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  sql = <<-SQL
+    SELECT *
+    FROM songs
+  SQL
+
+  DB = { conn: SQLite3::Database.new("db/music.db") }
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
 end
